94d8b2f20412d353b18f5195ac1372535e690fa3
Eugene Ravkovich <yravkovich@mail.ru>
2021-06-22 03:46:05 +0300

    Initial commit

---
+++ ArrayMean/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArrayMean
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            int[] numbers = new int[10];
+            Random randomNumber = new Random();
+            int sum = 0;
+            for(int i=0; i<numbers.Length; i++) 
+            {
+                numbers[i] = randomNumber.Next(10);
+                sum += numbers[i];
+                Console.Write(numbers[i] + "\t");
+            }
+            Console.WriteLine("\n" + (float)sum/numbers.Length);
+        }
+    }
+}
---
+++ SwapMinMaxElements/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace SwapMinMaxElements
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            int[] numbers = new int[] { 3, 4, 1, 10, 15, 6, 9, 20, 13, 7};
+            PrintMassive(numbers);
+            int minIndex = Array.IndexOf(numbers, numbers.Min());
+            int maxIndex = Array.IndexOf(numbers, numbers.Max());
+            int tmp = numbers[maxIndex];
+            numbers[maxIndex] = numbers[minIndex];
+            numbers[minIndex] = tmp;
+            PrintMassive(numbers);
+        }
+
+        static void PrintMassive(int[] arr) 
+        {
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
