117371eaf797e359b62328f2ee320d94fa34a261
Eugene Ravkovich <yravkovich@mail.ru>
2021-06-22 18:16:38 +0300

    Initial commit

---
+++ Cardealer/CarDealer/Car.cs
@@ -0,0 +1,24 @@
+﻿namespace Cardealer
+{
+    public class Car
+    {
+        public Brand Brand { get; private set; }
+        public string Model { get; private set; }
+        public BodyType BodyType { get; private set; }
+        public EngineType EngineType { get; private set; }
+        public double EngineCapacity { get; private set; }
+        public double Price { get; private set; }
+
+
+        public Car(Brand brand, string model, BodyType bodyType, EngineType engineType,
+                   double engineCapacity, double price) 
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.BodyType = bodyType;
+            this.EngineType = engineType;
+            this.EngineCapacity = engineCapacity;
+            this.Price = price;
+        }
+    }
+}
---
+++ Cardealer/Enums/BodyType.cs
@@ -0,0 +1,14 @@
+﻿namespace Cardealer
+{
+    public enum BodyType
+    {
+        Sedan,
+        StationWagon,
+        Hatchback,
+        Coupe,
+        Limousine,
+        Minivan,
+        Cabriolet,
+        Pickup
+    }
+}
