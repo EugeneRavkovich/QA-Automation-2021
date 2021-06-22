c1e4c9feac39e08ff6e54f97daf45b5175e5c9a9
Eugene Ravkovich <yravkovich@mail.ru>
2021-06-22 18:26:30 +0300

    Initial commit

---
+++ TheLongestSequence.Tests/UniqueSequenceFinderTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TheLongestSequence.Tests
+{
+    [TestClass]
+    public class UniqueSequenceFinderTests
+    {
+        [TestMethod]
+        [DataRow("tocom", 3)]
+        [DataRow("mmmmmm", 1)]
+        [DataRow("momomom", 2)]
+        [DataRow("12312", 3)]
+        [DataRow("1111", 1)]
+        [DataRow("    ", 1)]
+        public void GetSubstringLengthCheckPositive(string input, int expectedLength) 
+        {
+            Assert.AreEqual(expectedLength, UniqueSequenceFinder.GetSubstringLength(input));
+        }
+
+        [TestMethod]
+        [DataRow("tocomposite", 7)]
+        [DataRow("absdfetaj", 8)]
+        [DataRow("asporas", 5)]
+        [DataRow("arrownrra", 4)]
+        public void GetTheLongestUniqueSubsequenceCheckPositive(string input, int expectedLength)
+        {
+            Assert.AreEqual(expectedLength, UniqueSequenceFinder.GetTheLongestUniqueSubsequenceLength(input));
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetTheLongestUniqueSubsequenceWithEmptyInputNegative()
+        {
+            string.Empty.GetTheLongestUniqueSubsequenceLength();
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetTheLongestSubsequenceWithNullInputNegative() 
+        {
+            string input = null;
+            input.GetTheLongestUniqueSubsequenceLength();
+        }
+    }
+}
