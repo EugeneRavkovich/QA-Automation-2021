94719711b8db482649afe3d6ee7ea2dd2815f398
Eugene Ravkovich <yravkovich@mail.ru>
2021-06-22 03:58:27 +0300

    Initial commit

---
+++ Credentials/Credentials.cs
@@ -0,0 +1,29 @@
+﻿namespace Task1
+{
+    /// <summary>
+    /// A class that represents an credentials object
+    /// </summary>
+    public class Credentials
+    {
+        public string Login { get; private set; }
+        private string Password { get; set; }
+        private string SecretQuestion { get; set; }
+
+
+        public Credentials(string loginName, string password,
+            string secretQuestion)
+        {
+            this.Login = loginName;
+            this.Password = password;
+            this.SecretQuestion = secretQuestion;
+        }       
+
+
+        public bool IsEqual(Credentials creds)
+        {
+            return (Login == creds.Login
+                    && Password == creds.Password
+                    && SecretQuestion == creds.SecretQuestion);            
+        }
+    }
+}
---
+++ Credentials/Helper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Task1
+{
+    /// <summary>
+    /// Auxiliary static class
+    /// </summary>
+    public static class Helper
+    {
+        /// <summary>
+        /// Simulates the database
+        /// </summary>
+        /// <returns>Information about credentials from 'database'</returns>
+        public static Credentials[] GetCredentialsFromDB()
+        {
+            Credentials[] dataBaseCreds = new Credentials[3];
+            dataBaseCreds[0] = new Credentials("name1", "password1", "qwerty1");
+            dataBaseCreds[1] = new Credentials("name2", "password2", "qwerty2");
+            dataBaseCreds[2] = new Credentials("name3", "password3", "qwerty3");
+            return dataBaseCreds;
+        }
+
+
+        /// <summary>
+        /// Verify person with input credentials
+        /// </summary>
+        /// <param name="creds">User's credentials</param>
+        /// <returns>Does such user mentioned in database or not</returns>
+        public static bool IsExistingUser(Credentials creds)
+        {
+            Credentials[] dbCredentials = Helper.GetCredentialsFromDB();
+
+            foreach (Credentials credData in dbCredentials)
+            {
+                if (creds.IsEqual(credData))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public static void TryToLogIn(Credentials cred) 
+        {
+            if (IsExistingUser(cred)) 
+            {
+                LogIn(cred);
+            }
+            else 
+            {
+                throw new Exception("Unable to log in");
+            }
+        }
+
+
+        public static void LogIn(Credentials cred) 
+        {
+            //pass
+        }
+    }
+}
