2baf00dc10d7c00367063e45a99ae811b738e51e
Eugene Ravkovich <yravkovich@mail.ru>
2021-06-24 15:45:32 +0300

    Initial commit

---
+++ Selenium first prog/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_first_prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.onliner.by/";
+            IWebElement catalog = driver.FindElement(By.LinkText("Каталог"));
+            catalog.Click();
+            IWebElement electronics = driver.FindElement(By.XPath("//*[@data-id='1']"));
+            electronics.Click();
+            IWebElement mobilePhonesAndAccessories = driver.FindElement(By.ClassName("catalog-navigation-list__aside-title"));
+            mobilePhonesAndAccessories.Click();
+            IWebElement mobilePhones = driver.FindElement(By.XPath("//*[@class='catalog-navigation-list__dropdown-list']/a[1]"));
+            mobilePhones.Click();
+            String telephoneModel = driver.FindElement(By.XPath("//*[@data-bind='html: product.extended_name || product.full_name']")).Text;
+            String telephonePrice = driver.FindElement(By.XPath("//*[@class='schema-product__price-value schema-product__price-value_primary']/span")).Text;
+
+            Console.WriteLine("\nThe first mobile phone on the first page: \n"+ 
+                        "Model: " + telephoneModel + "\nPrice: " + telephonePrice);
+        }
+    }
+}
