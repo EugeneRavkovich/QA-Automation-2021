e1824b46310d80e0875f980bc21a114e3ea63163
Eugene Ravkovich <yravkovich@mail.ru>
2021-06-25 14:18:31 +0300

    Update converter tests

--- NumeralSystems/Converter.cs
+++ NumeralSystems/Converter.cs
@@ -6,9 +6,15 @@ namespace NumeralSystems
 {
     public static class Converter
     {
-        private static char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+        public static char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
 
 
+        /// <summary>
+        /// Converts a decimal number to the specified numeral system
+        /// </summary>
+        /// <param name="number">Decimal number for conversion</param>
+        /// <param name="systemBase">Numeral system base of resulting number</param>
+        /// <returns>Converted number in string</returns>
         public static string Convert(int number, int systemBase) 
         {
             if (systemBase < 2 || systemBase > 20) 
@@ -35,19 +41,38 @@ namespace NumeralSystems
         }
 
 
-        static char GetRemainder(int remainder) 
+        /// <summary>
+        /// Converts an int number to a string
+        /// </summary>
+        /// <param name="remainder">Result of X % Y expression</param>
+        /// <returns>Equivalent char or corresponding letter</returns>
+        private static char GetRemainder(int remainder) 
         {
-            return remainder >= 10 ? letters[remainder % 10] : (char)(remainder+'0');
+            //return remainder >= 10 ? letters[remainder % 10] : 
+            //       (char)(remainder + '0'); 
+            remainder = RemoveSign(remainder);
+            return remainder >= 10 ? letters[remainder % 10] :
+                   (char)(remainder + '0');
         }
 
 
-        static string GetSign(int number) 
+        /// <summary>
+        /// Detects the sign of a number
+        /// </summary>
+        /// <param name="number">Signed or unsigned number</param>
+        /// <returns>Minus or empty string</returns>
+        private static string GetSign(int number) 
         {
             return number < 0 ? "-" : string.Empty;
         }
 
 
-        static int RemoveSign(int number) 
+        /// <summary>
+        /// Removes the number sign
+        /// </summary>
+        /// <param name="number">Signed or unsigned number</param>
+        /// <returns>Unsigned number</returns>
+        private static int RemoveSign(int number) 
         {
             return number < 0 ? -number : number;
         }
