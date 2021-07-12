1bbd4626b5edf62cc16a244f046c739031c00942
Eugene Ravkovich <yravkovich@mail.ru>
2021-07-12 03:50:06 +0300

    Add Invoker class

---
+++ CarFleet/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿namespace CarFleet
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Invoker invoker = new Invoker();
+            invoker.Execute(new AddCommand(new Car("volvo", "s40", 3, 300)));
+            invoker.Execute(new AddCommand(new Car("volvo", "s30", 1, 100)));
+            invoker.Execute(new AddCommand(new Car("nissan", "skyline", 2, 200)));
+            invoker.Execute(new CountAllCommand());
+            invoker.Execute(new CountBrandsCommand());
+            invoker.Execute(new GetAveragePriceCommand());
+            invoker.Execute(new GetAveragePriceByBrandCommand("volvo"));
+            invoker.Execute(new ExitCommand());
+        }
+    }
+}
---
+++ CarFleet/Invokers/Invoker.cs
@@ -0,0 +1,14 @@
+﻿namespace CarFleet
+{
+    class Invoker
+    {
+        public void Execute(ICommand command)
+        {
+            if (command is null)
+            {
+                return;
+            }
+            command.Execute();
+        }
+    }
+}
