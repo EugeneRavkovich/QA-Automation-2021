9baa57f7141addac6a12bf3fad98307bf45fdddb
Eugene Ravkovich <yravkovich@mail.ru>
2021-07-12 12:52:32 +0300

    Add a class with instructions for the program

--- CarFleet/EntryPoint.cs
+++ CarFleet/EntryPoint.cs
@@ -5,14 +5,11 @@
         static void Main(string[] args)
         {
             Invoker invoker = new Invoker();
-            invoker.Execute(new AddCommand(new Car("volvo", "s40", 3, 300)));
-            invoker.Execute(new AddCommand(new Car("volvo", "s30", 1, 100)));
-            invoker.Execute(new AddCommand(new Car("nissan", "skyline", 2, 200)));
-            invoker.Execute(new CountAllCommand());
-            invoker.Execute(new CountBrandsCommand());
-            invoker.Execute(new GetAveragePriceCommand());
-            invoker.Execute(new GetAveragePriceByBrandCommand("volvo"));
-            invoker.Execute(new ExitCommand());
+            Utility.DisplayWelcomeMessage();
+            while (true)
+            {
+                invoker.Execute(Parser.Parse(System.Console.ReadLine().Split(' ')));
+            }
         }
     }
 }
---
+++ CarFleet/Utilities/Utility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CarFleet
+{
+    static class Utility
+    {
+        public static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("This is a program for car fleet managment\n" +
+                "********************************************************************************\n" +
+                "Available commands:\n" +
+                "================================================================================\n" +
+                "add: add a car to the fleet\n" +
+                "--------------------------------------------------------------------------------\n" +
+                "add - brand - model - quantity - cost of one unit\n" +
+                "Example: add subaru wrx 1 12500\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "count all: count the total number of cars\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "count brands: count the total number of car brands\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "average price: calculate the average price of all cars\n" +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "average price brand: calculate the average price of all cars of a specified brand\n" +
+                "--------------------------------------------------------------------------------\n" +
+                "average price brand - car brand\n" +
+                "Example: average price brand subaru\n" +
+                "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+        }
+
+    }
+}
