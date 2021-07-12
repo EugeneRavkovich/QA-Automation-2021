35007dac6d9e5ff8f3f2ba48c8a675953d41fa92
Eugene Ravkovich <yravkovich@mail.ru>
2021-07-12 03:11:33 +0300

    Add Car and CarFleet classes

---
+++ CarFleet/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CarFleet
+{
+    class Car
+    {
+        private int _quantity;
+
+        private int _price;
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Quantity
+        {
+            get
+            {
+                return _quantity;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("The number of cars can't be negative");
+                }
+                _quantity = value;
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Price can't be negative");
+                }
+                _price = value;
+            }
+        }
+
+
+        public Car(string brand, string model, int quantity, int price)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Quantity = quantity;
+            this.Price = price;
+        }
+    }
+}
---
+++ CarFleet/CarFleet.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CarFleet
+{
+    class CarFleet
+    {
+        private static CarFleet _fleet;
+
+        public List<Car> park;
+
+
+        private CarFleet()
+        {
+            this.park = new List<Car>();
+        }
+
+
+        public static CarFleet GetFleet()
+        {
+            if (_fleet is null)
+            {
+                _fleet = new CarFleet();
+            }
+            return _fleet;
+        }
+
+
+        public void Add(Car car)
+        {
+            for (int i = 0; i < car.Quantity; i++)
+            {
+                park.Add(car);
+            }
+        }
+
+
+        public int CountBrands()
+        {
+            return park.Select(x => x.Brand).Distinct().Count();
+        }
+
+
+        public int CountAll()
+        {
+            return park.Count;
+        }
+
+
+        public double GetAveragePrice()
+        {
+            return park.Sum(x => x.Price) / (double)park.Count;
+        }
+
+
+        public double GetAveragePriceByBrand(string brand)
+        {
+            var selection = park.Where(x => x.Brand == brand).ToList();
+            return selection.Sum(x => x.Price) / (double)selection.Count;
+        }
+    }
+}
