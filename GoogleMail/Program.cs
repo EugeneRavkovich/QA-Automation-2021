122f29fe9ed5307aad0699bc90ee6ee3a07bc558
Eugene Ravkovich <yravkovich@mail.ru>
2021-07-26 10:15:35 +0300

    Add Gmail Program class

---
+++ GoogleMail/Program.cs
@@ -0,0 +1,27 @@
+﻿using GoogleMail.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using GoogleMail.Pages;
+using System;
+
+namespace GoogleMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GmailHelper.InitBrowser();
+            InboxPage ip = (InboxPage)GmailHelper.DoLogin("qwerty7312oo", "password1_8pass");
+            //GmailHelper.SendMessage("gaaasmanov@mail.ru", "1231");
+            //ip.OpenMessage();
+            
+            Console.WriteLine(ip.IsMessageNotRead());
+            Console.WriteLine(ip.IsCorrectAddressee("Олег Газманов"));
+            //ip.OpenMessage();
+            //Console.WriteLine(ip.GetMessageContent());
+            ip.OpenAvailableAccountsTab();
+            AccountPage ap = ip.OpenAccountSettings();
+            ap.OpenPersonalInformationTab();
+        }
+    }
+}
