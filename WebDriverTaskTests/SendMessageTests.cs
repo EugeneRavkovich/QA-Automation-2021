fb2aba7262d9f59650e45396a8e70d9b44f09130
Eugene Ravkovich <yravkovich@mail.ru>
2021-07-26 10:18:52 +0300

    Add SendMessageTests class

---
+++ WebDriverTaskTests/SendMessageTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MailRu.Helpers;
+using GoogleMail.Helpers;
+
+namespace WebDriverTaskTests
+{
+    [TestClass]
+    public class SendMessageTests
+    {
+        private const string MailRuUsername = "gaaasmanov@mail.ru";
+
+        private const string MailRuPassword = "password1_9pass";
+
+        private const string GmailUsername = "qwerty7312oo";
+
+        private const string GmailPassword = "password1_8pass";
+
+        private const string GmailUserAlias = "Антон Антонов";
+
+        private const string MessageContent = "Hello! World!";
+
+
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            GmailHelper.InitBrowser();
+            GmailHelper.DoLogin(GmailUsername, GmailPassword);
+            GmailHelper.SendMessage(MailRuUsername, MessageContent);
+
+            MailRuHelper.InitBrowser();
+            MailRuHelper.DoLogin(MailRuUsername, MailRuPassword);
+        }
+
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            MailRuHelper.CloseBrowser();
+            GmailHelper.CloseBrowser();
+        }
+
+
+        [TestMethod]
+        public void IsCorrectMessage()
+        {
+            bool isMessageCame = MailRuHelper.IsMessageCame(GmailUserAlias, MessageContent);
+            Assert.IsTrue(isMessageCame);
+        }
+    }
+}
