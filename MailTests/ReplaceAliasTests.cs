5b3ecbe139994ef4a21fee02f01e61d345cabd90
Eugene Ravkovich <yravkovich@mail.ru>
2021-08-13 16:34:42 +0300

    Add a ReplaceAliasTests class

---
+++ MailTests/ReplaceAliasTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using MailFramework.Models;
+
+namespace MailTests
+{
+    [TestFixture]
+    public class ReplaceAliasTests: TestsBase
+    {
+        private Alias baseAlias;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            gmailHelper.InitBrowser();
+            gmailHelper.DoLogin(gmailUser);
+            baseAlias = gmailHelper.GetCurrentAlias();
+            gmailHelper.SendMessage(mailruUser, messageContent1);
+
+            mailruHelper.InitBrowser();
+            mailruHelper.DoLogin(mailruUser);
+            mailruHelper.IsMessageCame(gmailUser, messageContent1);
+            mailruHelper.SendReplyToTheLastIncommingLetter(replyMessage);
+
+            gmailHelper.IsMessageCame(mailruUser);
+            gmailHelper.ReplaceAlias(gmailHelper.GetAliasFromMessage());
+        }
+
+
+        [TearDown]
+        public override void TearDown()
+        {
+            base.TearDown();
+            gmailHelper.ReplaceAliasBack(baseAlias);
+            gmailHelper.CloseBrowser();
+            mailruHelper.CloseBrowser();
+        }
+
+
+        [Test]
+        [Category("All")]
+        public void ReplaceAliasTest()
+        {
+            bool isUpdated = gmailHelper.IsAliasUpdated(baseAlias);
+            Assert.IsTrue(isUpdated);
+        } 
+    }
+}
