91dabdbb7ddbbb2b9f7ffca65b30942e57fa33c6
Eugene Ravkovich <yravkovich@mail.ru>
2021-08-14 02:35:28 +0300

    Add comments to the BasePage class

--- MailFramework/Models/BasePage.cs
+++ MailFramework/Models/BasePage.cs
@@ -3,15 +3,31 @@ using OpenQA.Selenium.Support.UI;
 
 namespace MailFramework.Models
 {
+    /// <summary>
+    /// Class that defines the entity of a BasePage for inheritance
+    /// </summary>
     public abstract class BasePage
     {
+        /// <summary>
+        /// The maximum WebElement waiting time, in seconds
+        /// </summary>
         private readonly int _waitTime = 15;
 
+        /// <summary>
+        /// Method for setting and getting a WebDriverWait object
+        /// </summary>
         public WebDriverWait Wait { get; private set; }
 
+        /// <summary>
+        /// Method for setting and getting the WebDriver object
+        /// </summary>
         public IWebDriver Driver { get; protected set; }
 
 
+        /// <summary>
+        /// Constructor for initializing the class fields
+        /// </summary>
+        /// <param name="driver">Selenium WebDriver object</param>
         public BasePage(IWebDriver driver)
         {
             this.Driver = driver;
