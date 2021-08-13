e054fdd6ceeb384f168d8b0bb91e14aa795028bc
Eugene Ravkovich <yravkovich@mail.ru>
2021-08-13 16:31:18 +0300

    Add a TestDataReader class

---
+++ MailFramework/Utilities/TestDataReader.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System.IO;
+using System.Linq;
+
+namespace MailFramework.Utilities
+{
+    public class TestDataReader
+    {
+        private static readonly string _filePath = "MailFramework/Resources/";
+        //private static readonly string _filePath = "../../Resources/";
+
+        private static readonly string _env = TestContext.Parameters["environment"];
+        //private static readonly string _env = "qa";
+
+        public static string GetTestData(string key)
+        {
+            return GetData(key);
+        }
+
+
+        private static string GetData(string key)
+        {
+            var root = JObject.Parse(File.ReadAllText(_filePath + _env + ".json"));
+            return root.Descendants().
+                    OfType<JProperty>().
+                    Where(x => x.Name == key).
+                    First().Value.ToString();
+        } 
+    }
+}
