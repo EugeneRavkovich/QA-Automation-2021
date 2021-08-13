53a687d1b0b73dd1d60b3fee5860f3c2ec30a74e
Eugene Ravkovich <yravkovich@mail.ru>
2021-08-13 16:13:54 +0300

    Add an Alias class

---
+++ MailFramework/Models/Alias.cs
@@ -0,0 +1,24 @@
+﻿namespace MailFramework.Models
+{
+    public class Alias
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+
+        public Alias(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            Alias comparableAlias = (Alias)obj;
+            return this.Name == comparableAlias.Name &&
+                this.Surname == comparableAlias.Surname;
+        }
+    }
+}
