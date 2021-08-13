0234c6df5e7d58de15cdc7008fa2120d7eff49e8
Eugene Ravkovich <yravkovich@mail.ru>
2021-08-13 16:16:04 +0300

    Add a gmail AccountPage class

---
+++ MailFramework/MailServices/Gmail/Pages/AccountPage.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using NLog;
+
+namespace MailFramework.MailServices.Gmail.Pages
+{
+    public class AccountPage: BasePage
+    {
+        private readonly By _personalInformationLocator = By.XPath("(//a[@href='personal-info?gar=1'])[2]");
+
+        private readonly By _nameFieldLocator = By.XPath("//a[@href='name?gar=1']");
+
+        private readonly string _driverTitle = "Аккаунт Google";
+
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+
+        public AccountPage(IWebDriver driver) : base(driver)
+        {
+            Driver.SwitchTo().Window(Driver.WindowHandles[1]);
+            Wait.Until(ExpectedConditions.TitleIs(_driverTitle));
+            _logger.Info("The account configuration page is loaded");
+        }
+
+
+        public AccountPage OpenPersonalInformationTab()
+        {
+            Wait.Until(ExpectedConditions.ElementIsVisible(_personalInformationLocator));
+            Driver.FindElement(_personalInformationLocator).Click();
+            return this;
+        }
+
+
+        public ChangeNameTab OpenChangeNameTab()
+        {
+            Wait.Until(ExpectedConditions.ElementIsVisible(_nameFieldLocator));
+            Driver.FindElement(_nameFieldLocator).Click();
+            return new ChangeNameTab(Driver);
+        }
+    }
+}
