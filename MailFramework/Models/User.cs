cf5b7ce83ff088cb1c987da7250814c7a7a179c6
Eugene Ravkovich <yravkovich@mail.ru>
2021-08-13 16:13:35 +0300

    Add an User class

---
+++ MailFramework/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace MailFramework.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+
+        public User(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.Username}\n{this.Password}";
+        }
+    }
+}
