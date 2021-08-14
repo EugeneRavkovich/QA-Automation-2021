9ccab70def9f7f298d62fa255971947884f29c28
Eugene Ravkovich <yravkovich@mail.ru>
2021-08-15 00:40:00 +0300

    Add an ExpectedConditionsWrapper class

---
+++ MailFramework/Wrappers/ExpectedConditionsWrapper.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace MailFramework.Wrappers
+{
+    public class ExpectedConditionsWrapper
+    {
+        public static Func<IWebDriver, IWebElement> ElementIsVisible(By locator)
+        {
+            return driver => 
+            {
+                try
+                {
+                    IWebElement tempElement = driver.FindElement(locator);
+                    return tempElement.Displayed ? tempElement : null;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return null;
+                }
+            };
+        }
+    }
+}
